9c1e977049e18f4f6896c6a5742ee58d3ab46b8d
hossamabohadima <hossamabohadima@gmail.com>
2026-03-02 17:40:28 +0200

    Create start tables

---
+++ DataBase/Stored Procedures.sql
@@ -0,0 +1,48 @@
+CREATE OR REPLACE PROCEDURE register_user(
+    p_name VARCHAR,
+    p_email VARCHAR,
+    p_password TEXT,
+    p_phone VARCHAR,
+    p_role user_role,
+    p_initial_threshold DECIMAL(5,2)
+)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    new_userid INT;
+BEGIN
+    -- 1. Insert into Users and get the new ID
+    INSERT INTO Users (name, email, password, phoneNumber, role)
+    VALUES (p_name, p_email, p_password, p_phone, p_role)
+    RETURNING userid INTO new_userid;
+
+    -- 2. Initialize Threshold
+    INSERT INTO Threshold (userID, thresholdValue)
+    VALUES (new_userid, p_initial_threshold);
+
+    -- 3. Initialize MetricWeight with default 1.0 (or 0.0)
+    INSERT INTO MetricWeight (userID, SpeechClarity, SpeechFluency, SpeechConfidence)
+    VALUES (new_userid, 0, 1.0, 0); 
+
+    COMMIT;
+END;
+$$;
+
+
+CREATE OR REPLACE FUNCTION add_video(
+    p_video_name VARCHAR,
+    p_user_id INT,
+    p_duration DECIMAL(5,2),
+    p_status videoStatus
+) RETURNS INT AS $$
+DECLARE
+    new_video_id INT;
+BEGIN
+    -- 1. Insert Video record
+    INSERT INTO Video (VideoName, userID, duration, status)
+    VALUES (p_video_name, p_user_id, p_duration, p_status)
+    RETURNING videoID INTO new_video_id;
+
+    RETURN new_video_id;
+END;
+$$ LANGUAGE plpgsql;
---
+++ DataBase/create tables.sql
@@ -0,0 +1,60 @@
+-- 1. Create the custom type 
+CREATE TYPE user_role AS ENUM ('USER', 'RECRUITER');
+CREATE TYPE videoStatus AS ENUM ('DONE', 'FAILED');
+
+-- 2. Create Users table
+CREATE TABLE Users (
+    userid SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    phoneNumber VARCHAR(20), 
+    role user_role NOT NULL 
+);
+-- 3. Create Threshold table
+CREATE TABLE Threshold (
+    userID int PRIMARY KEY REFERENCES Users(userid) ON DELETE CASCADE,
+    thresholdValue DECIMAL(5,2) NOT NULL
+);
+-- 4. Create MetricWeight table
+CREATE TABLE MetricWeight (
+    userID int PRIMARY KEY REFERENCES Users(userid) ON DELETE CASCADE,
+   SpeechClarity DECIMAL(5,2) default 0,
+   SpeechFluency DECIMAL(5,2) default 0,
+   SpeechConfidence DECIMAL(5,2) default 0,
+   SpeechExpressiveness DECIMAL(5,2) default 0,
+   SpeechEngagement DECIMAL(5,2) default 0,
+   FacialConfidence DECIMAL(5,2) default 0,
+   FacialApproachability DECIMAL(5,2) default 0,
+   FacialEngagement DECIMAL(5,2) default 0,
+   VideoProfessionalism DECIMAL(5,2) default 0
+    CONSTRAINT metric_score_limit 
+    CHECK (SpeechClarity + SpeechFluency + SpeechConfidence + SpeechExpressiveness + SpeechEngagement + FacialConfidence + FacialApproachability + FacialEngagement + VideoProfessionalism ==1)
+
+);
+-- 5. Create Video table
+CREATE TABLE Video (
+    videoID SERIAL PRIMARY KEY,
+    VideoName VARCHAR(50) NOT NULL,
+    userID int REFERENCES Users(userid) ON DELETE CASCADE,
+    uploadDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    duration DECIMAL(5,2) ,
+    status videoStatus NOT NULL 
+);
+-- 6. Create videoScore table
+CREATE TABLE videoScore (
+    videoID int PRIMARY KEY REFERENCES Video(videoID) ON DELETE CASCADE,
+   SpeechClarity DECIMAL(5,2) default 0,
+   SpeechFluency DECIMAL(5,2) default 0,
+   SpeechConfidence DECIMAL(5,2) default 0,
+   SpeechExpressiveness DECIMAL(5,2) default 0,
+   SpeechEngagement DECIMAL(5,2) default 0,
+   FacialConfidence DECIMAL(5,2) default 0,
+   FacialApproachability DECIMAL(5,2) default 0,
+   FacialEngagement DECIMAL(5,2) default 0,
+   VideoProfessionalism DECIMAL(5,2) default 0,
+   totalScore DECIMAL(5,2) default 0,
+    CONSTRAINT score_limit 
+    CHECK (totalScore >= 0 AND totalScore <= 100)
+   ); 
