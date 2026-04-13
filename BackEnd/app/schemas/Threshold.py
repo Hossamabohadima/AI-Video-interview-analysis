4422e0a65db84b9c33760162ed2a3d70d0f48cd8
hossamabohadima <hossamabohadima@gmail.com>
2026-04-14 00:32:30 +0200

    check  back end and accept

--- BackEnd/app/schemas/Threshold.py
+++ BackEnd/app/schemas/Threshold.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, Field
 
-class RecruiterSetThreshold(BaseModel):
+class Threshold(BaseModel):
     user_id: int = Field(..., gt=0)
     score: float = Field(..., ge=0, le=1)
--- BackEnd/app/services/user_service.py
+++ BackEnd/app/services/user_service.py
@@ -1,45 +1,95 @@
+from altair import Dict
+
 from db import get_db_connection
 
-def register_user(user):
+from fastapi import UploadFile
+from moviepy import VideoFileClip
+import uuid, os, shutil
+
+
+
+def upload_video(file: UploadFile, user_id: int, video_name: str):
+    folder = "videos"
+    os.makedirs(folder, exist_ok=True)
+
+    ext = os.path.splitext(file.filename)[1]
+    unique_name = video_name + "_" + str(uuid.uuid4()) + ext
+    output_path = os.path.join(folder, unique_name)
+
+    duration = 0.0
+
+    try:
+        with open(output_path, "wb") as f:
+            shutil.copyfileobj(file.file, f)
+
+        try:
+            with VideoFileClip(output_path) as video:
+                duration = video.duration
+        except Exception as e:
+            print("Error extracting duration:", e)
+
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        cur.execute(
+            "SELECT add_video(%s, %s, %s, %s)",
+            (unique_name, user_id, duration, "PENDING")
+        )
+
+        rec = cur.fetchone()[0]
+        conn.commit()
+
+        cur.close()
+        conn.close()
+
+
+        return {
+            "video_id": rec["video_id"],
+            "file_name": unique_name,
+            "duration": duration,
+            "status": "DONE"
+        }
+
+    except Exception as e:
+        if os.path.exists(output_path):
+            os.remove(output_path)
+        raise e
+
+
+def get_reports(user_id):
     conn = get_db_connection()
     cur = conn.cursor()
 
-    cur.execute(
-        "CALL register_user(%s, %s, %s, %s, %s, %s)",
-        (user.name, user.email, user.password, user.phoneNumber, user.role, user.initial_threshold)
-    )
+    cur.execute("SELECT * FROM get_reports_sp(%s)", (user_id,))
+    data = cur.fetchall()
 
-    conn.commit()
     cur.close()
     conn.close()
+    return data
 
 
-def login_user(data):
+def compare_reports(v1, v2):
     conn = get_db_connection()
     cur = conn.cursor()
 
-    cur.execute("SELECT * FROM login_user_sp(%s, %s)",
-                (data.email, data.password))
-
-    user = cur.fetchone()
-    print(user)
-
+    cur.execute("SELECT * FROM compare_reports_sp(%s, %s)", (v1, v2))
+    data = cur.fetchall()
 
     cur.close()
     conn.close()
-    return user
+    return data
 
 
-def set_weights(req):
+def set_weights(user_id: int, weights: Dict[str, float]):
     conn = get_db_connection()
     cur = conn.cursor()
 
-    w = req.weights
+    w = weights
 
     cur.execute("""
         CALL set_weights_sp(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
     """, (
-        req.user_id,
+        user_id,
         w["SpeechClarity"],
         w["SpeechFluency"],
         w["SpeechConfidence"],
@@ -50,6 +100,16 @@ def set_weights(req):
         w["FacialEngagement"],
         w["VideoProfessionalism"]
     ))
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def set_threshold_score(user_id: int, score: float):
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute("CALL set_threshold_score(%s, %s)", (user_id, score))
 
     conn.commit()
     cur.close()
